1e14a35b8040402e950b1d3cdf814dd0d943b2be
gvieri <g.vieri@gmail.com>
2020-02-28 19:22:47 +0100

    Added new features: ratio calc death and recovered /confirmed ratio. save results in csv format

--- README.md
+++ README.md
@@ -4,5 +4,20 @@ Stand for Corona Virus Utilities. I want to have some data in tables. So I writt
 
 License: GPL v 2.0.
 
+
+I have added some features. First one the help:
+python covid19glodata.py --help 
+or:
+python covid19glodata.py -h . 
+
+So now you can save the aggregate data, and/or you can add Death/Confirmed % ratio AND the recovered/Confirmed ratio % . 
+Try:
+python covid19glodata.py --ratio 
+or
+python covid19glodata.py --ratio --save 
+and look for result in out.csv (it is a csv file so, you postprocess in excel or whatsoever you like). 
+
+Stay tuned: I'm planning to add some chart and basic trend analisys. Let me know if you like the idea. 
+
 Enjoy. 
 
--- covid19glodata.py
+++ covid19glodata.py
@@ -7,10 +7,22 @@ from git import Repo
 from os import listdir 
 import csv
 import os
+import sys
 import shutil
+import argparse 
+import numpy as np
 
 destdir="covid19"
 searchdir=destdir+"/csse_covid_19_data/csse_covid_19_daily_reports"
+
+def getOptions(args=sys.argv[1:]):
+    parser=argparse.ArgumentParser(description='fetch official COVID-19 data from github and, make aggregate set')
+    parser.add_argument('-r','--ratio', help='add deaths/confirmed and recovered/confirmed ratio columns', action='store_true')
+    parser.add_argument('-s','--save', help='save results in a file (without header)', action='store_true' ) 
+    parser.add_argument('-d','--debug',help='enables debug info', action='store_true' )
+    opt=parser.parse_args(args)
+    return(opt) 
+
 
 def processafile(filename):
     completefile=searchdir+"/"+filename
@@ -27,24 +39,46 @@ def processafile(filename):
             recovered +=int(row[5])
     return(confirmed,deaths,recovered)
 
+##########################################
+if __name__ == "__main__":
+    opt=getOptions()
+    ratio=opt.ratio
+    save =opt.save
+    if os.path.isdir(destdir):
+        shutil.rmtree(destdir)
 
-if os.path.isdir(destdir):
-    shutil.rmtree(destdir)
+    os.mkdir(destdir)
 
-os.mkdir(destdir)
-
-Repo.clone_from("https://github.com/CSSEGISandData/COVID-19",destdir)
+    Repo.clone_from("https://github.com/CSSEGISandData/COVID-19",destdir)
 
 # now search all csv files in destdir/csse_covid_19_data/csse_covid_19_daily_reports
 
-ext='csv' 
+    ext='csv' 
 
-filenames=listdir(searchdir)
-filenames.sort()
+    filenames=listdir(searchdir)
+    filenames.sort()
+    orig_stdout=sys.stdout
 
-for filename in filenames:
-   if filename.endswith(ext):
-       r=processafile(filename) 
-       print("{},{},{},{}".format(filename[:-4],r[0],r[1],r[2]) )
-
-
+    if save:
+        fo=open('out.csv','w')
+        sys.stdout=fo
+        
+    for filename in filenames:
+       if filename.endswith(ext):
+           r=list(processafile(filename) )
+           if r[1] >0:
+                r.append(float(r[1]/r[0]))
+           else:
+                r.append('NaN')
+           if r[2] >0:
+                r.append(float(r[2]/r[0]))
+           else:
+                r.append('NaN')
+           
+           if ratio:
+                print("{},{},{},{},{:.3f},{:.3f}".format(filename[:-4],r[0],r[1],r[2],r[3],r[4]) )
+           else: 
+                print("{},{},{},{}".format(filename[:-4],r[0],r[1],r[2]) )
+    if save:
+        sys.stdout=orig_stdout
+        fo.close()
